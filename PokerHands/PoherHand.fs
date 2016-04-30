74cb2cc1f1701cac50df01336b7bdf8de9e3702c
bruno <bruno.belmondo@gmail.com>
2016-04-30 18:36:25 +0200

    correction de la suite et ajout de cas simples de victoire

--- PokerHands/PoherHand.fs
+++ PokerHands/PoherHand.fs
@@ -6,22 +6,26 @@ module PokerHand =
                 |Spade
                 |Clubs
 
-    type Value =  |Two
-                  |Three
-                  |Four
-                  |Five
-                  |Six
-                  |Seven
-                  |Height
-                  |Nine
-                  |Ten
-                  |Jack
-                  |Queen
-                  |King
-                  |Ace
+    type Value =  |Two = 2
+                  |Three = 3
+                  |Four = 4
+                  |Five = 5
+                  |Six = 6
+                  |Seven = 7
+                  |Height = 8
+                  |Nine = 9
+                  |Ten = 10
+                  |Jack = 11
+                  |Queen = 12
+                  |King = 13
+                  |Ace = 14
 
     type Card = {Value:Value; Suit:Suit}
      
+    type Hand = {Cards :Card list}
+
+    type Player = {Name: string; Hand: Hand}
+
     let parseSuit char =
         match char with
         | 'H' -> Heart
@@ -32,22 +36,23 @@ module PokerHand =
      
     let parseValue char = 
         match char with
-        |'2'-> Two
-        |'3'-> Three
-        |'4'-> Four
-        |'5'-> Five
-        |'6'-> Six
-        |'7'-> Seven
-        |'8'-> Height
-        |'9'-> Nine
-        |'T'-> Ten
-        |'J'-> Jack
-        |'Q'-> Queen
-        |'K'-> King
-        |'A'-> Ace
+        |'2'-> Value.Two
+        |'3'-> Value.Three
+        |'4'-> Value.Four
+        |'5'-> Value.Five
+        |'6'-> Value.Six
+        |'7'-> Value.Seven
+        |'8'-> Value.Height
+        |'9'-> Value.Nine
+        |'T'-> Value.Ten
+        |'J'-> Value.Jack
+        |'Q'-> Value.Queen
+        |'K'-> Value.King
+        |'A'-> Value.Ace
 
 
     let getValue card = card.Value
+    let getNumericValue card = int card.Value
     let getSuit card = card.Suit
 
     let parseCard valueToParse suitToParse =
@@ -59,22 +64,30 @@ module PokerHand =
         match Seq.toList token with
         | v::s::[] -> parseCard v s
 
-    type Hand = {Cards :Card list}
+    let isNotEmpty (text:string) = text.Trim().Length > 0
 
     let parseHand (text:string) = 
-        let cards = Array.toList (text.Split(' ')) 
+        let cards = Array.toList (text.Split(' '))
+                    |> List.filter isNotEmpty
                     |> List.map parseCardText
         {Cards=cards}
 
-    type Score = |HighCard
-                 |Pair
-                 |TwoPairs
-                 |ThreeOfAKind
-                 |Straight
-                 |Flush
-                 |FullHouse
-                 |FourOfAKind
-                 |StraightFlush
+    let parsePlayer (input:string) =
+         let tokens = input.Split(':')
+         let name = string (tokens.GetValue(0))
+         let handInput = string (tokens.GetValue(1))
+         let hand = parseHand handInput
+         {Name=name;Hand=hand}
+
+    type Score = |HighCard = 0
+                 |Pair = 1
+                 |TwoPairs = 2
+                 |ThreeOfAKind = 3
+                 |Straight = 4
+                 |Flush = 5
+                 |FullHouse = 6
+                 |FourOfAKind = 7
+                 |StraightFlush = 8
 
     let groupCardsByValue hand = Seq.groupBy getValue hand.Cards
     let groupCardsBySuit hand = Seq.groupBy getSuit hand.Cards
@@ -120,14 +133,78 @@ module PokerHand =
        | 1 -> Some IsFlush
        | _ -> None
 
+    type StraightChecker = {Valid:bool;Previous:int}
+
+    let checkItemFollows state item = 
+        let valid = state.Valid && item-state.Previous = 1
+        {Valid=valid;Previous=item}
+
+    let (|IsStraight|_|) hand =
+        let cardsValues = Seq.map getNumericValue hand.Cards
+        let firstCard = Seq.head cardsValues
+        let checkStraight = cardsValues
+                            |> Seq.skip 1
+                            |> Seq.fold checkItemFollows {Valid=true;Previous=firstCard}
+        if checkStraight.Valid then
+            Some IsStraight
+        else 
+            None                        
+
+
+    let (|IsStraightFlush|_|) hand =
+        let isStraight = match hand with
+                         |IsStraight -> true
+                         |_->false
+        let isFlush = match hand with
+                         |IsFlush -> true
+                         |_->false
+        if isStraight && isFlush then          
+            Some IsStraightFlush
+        else
+            None
+
+
     let computeScore hand = 
         match hand with
-        | IsFourOfAKind -> FourOfAKind
-        | IsFullHouse -> FullHouse
-        | IsFlush -> Flush
-        | IsThreeOfAKind -> ThreeOfAKind
-        | IsTwoPairs -> TwoPairs
-        | IsPair -> Pair
-        | _ -> HighCard
+        | IsStraightFlush -> Score.StraightFlush
+        | IsFourOfAKind -> Score.FourOfAKind
+        | IsFullHouse -> Score.FullHouse
+        | IsFlush -> Score.Flush
+        | IsStraight -> Score.Straight
+        | IsThreeOfAKind -> Score.ThreeOfAKind
+        | IsTwoPairs -> Score.TwoPairs
+        | IsPair -> Score.Pair
+        | _ -> Score.HighCard
 
     let computeScoreFromText = parseHand >> computeScore
+
+    type ScoredPlayer = {Name:string;Score:Score;Hand:Hand}
+
+    let scorePlayer (player:Player) = { 
+                                Name = player.Name;
+                                Score = computeScore player.Hand;
+                                Hand = player.Hand
+                             }
+    let parseAndComputeScore = parsePlayer >> scorePlayer
+
+    let printScore score =
+        match score with
+        |Score.HighCard -> "high card"
+        |Score.Pair -> "pair"
+        |Score.TwoPairs -> "two pairs"
+        |Score.ThreeOfAKind  -> "three of a kind"
+        |Score.Straight -> "straight"
+        |Score.Flush -> "flush"
+        |Score.FullHouse -> "full house"
+        |Score.FourOfAKind -> "four of a kind"
+        |Score.StraightFlush -> "straight flush"
+
+
+    let findWinner (input:string) = 
+        let playersInput = input.Split('\t')
+        let player1 = parseAndComputeScore (string (playersInput.GetValue(0)))
+        let player2 = parseAndComputeScore (string (playersInput.GetValue(1)))
+        match player1, player2 with
+        | p1, p2 when p1.Score > p2.Score -> p1.Name + " wins. - with " + (printScore p1.Score)
+        | p1, p2 when p2.Score > p1.Score -> p2.Name + " wins. - with " + (printScore p2.Score)
+        | _,_ -> "Tie."
--- PokerHands/PokerHandTests.fs
+++ PokerHands/PokerHandTests.fs
@@ -7,44 +7,54 @@ open PokerHand
 [<Test>]
 let ``should Find High Card when nothing special`` () = 
     let obtained = computeScoreFromText "2D 3H 4C 7S AD"
-    Assert.AreEqual(HighCard, obtained)
+    Assert.AreEqual(Score.HighCard, obtained)
 
 [<Test>]
 let ``should Find Pair when two cards have the same value`` () = 
     let obtained = computeScoreFromText "2D 3H 4C AS AD"
-    Assert.AreEqual(Pair, obtained)
+    Assert.AreEqual(Score.Pair, obtained)
 
 [<Test>]
 let ``should Find Two Pairs when two cards have the same value two times`` () = 
     let obtained = computeScoreFromText "2D 4H 4C AS AD"
-    Assert.AreEqual(TwoPairs, obtained)
+    Assert.AreEqual(Score.TwoPairs, obtained)
 
 [<Test>]
 let ``should Find Three of a kind when three cards have the same value`` () = 
     let obtained = computeScoreFromText "2D 3H AC AS AD"
-    Assert.AreEqual(ThreeOfAKind, obtained)
+    Assert.AreEqual(Score.ThreeOfAKind, obtained)
 
 [<Test>]
 let ``should Find four of a kind when four cards have the same value`` () = 
     let obtained = computeScoreFromText "2D AH AC AS AD"
-    Assert.AreEqual(FourOfAKind, obtained)
+    Assert.AreEqual(Score.FourOfAKind, obtained)
 
 [<Test>]
 let ``should Find full house when three cards have same value and two other are a pair`` () = 
     let obtained = computeScoreFromText "2D 2H AC AS AD"
-    Assert.AreEqual(FullHouse, obtained)
+    Assert.AreEqual(Score.FullHouse, obtained)
 
 [<Test>]
 let ``should Find flush when all cards have the same suit`` () = 
     let obtained = computeScoreFromText "2D 3D 6D TD AD"
-    Assert.AreEqual(Flush, obtained)
+    Assert.AreEqual(Score.Flush, obtained)
 
 [<Test>]
 let ``should Find straight when all cards follow each other`` () = 
     let obtained = computeScoreFromText "2D 3D 4H 5D 6D"
-    Assert.AreEqual(Straight, obtained)
+    Assert.AreEqual(Score.Straight, obtained)
 
 [<Test>]
 let ``should Find straight flush when all cards follow each other and have the same suit`` () = 
     let obtained = computeScoreFromText "2D 3D 4D 5D 6D"
-    Assert.AreEqual(StraightFlush, obtained)
+    Assert.AreEqual(Score.StraightFlush, obtained)
+
+[<Test>]
+let ``should Find winner when player do not have the same kind of score with flash`` () = 
+    let obtained = findWinner "Black: 2H 4S 4C 2D 8H\tWhite: 2S 8S AS QS 3S"
+    Assert.AreEqual("White wins. - with flush", obtained)
+
+[<Test>]
+let ``should Find winner when player do not have the same kind of score with three of a kind`` () = 
+    let obtained = findWinner "Black: 2H 4S 4C 2D 8H\tWhite: 2S 8S AS AD AC"
+    Assert.AreEqual("White wins. - with three of a kind", obtained)
